0be7070a2bf0b0b546d4d4adef033c78e652832b
Jaggar Henzerling <36236449+jhenzerling@users.noreply.github.com>
2018-03-21 15:35:38 +0000

    Add files via upload

---
+++ Logging/mod_log.py
@@ -0,0 +1,46 @@
+#Module for Recording and Logging
+
+import sys
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+#MNIST plotter to check inputs
+def MNIST_plot(trainbatch,hsize,vsize,number):
+    #PLOTTING#
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+    #print(np.argmax(trainbatch[0][number]))
+    img_data = (trainbatch[0][number].eval(session = sess)).reshape([hsize,vsize])
+    plt.imshow(img_data)
+    plt.show()
+    print('Which has class: ' + str(trainbatch[1][number].eval(session=sess)))
+
+#Main difference compared to MNISt is requirement of queues to deal with batches
+def CIFAR10_plot(trainbatch,hsize,vsize,number):
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord = coord)
+
+        #Initialize the nodes and variables
+        sess.run(tf.global_variables_initializer())
+        img,lab = sess.run([trainbatch[0][number],trainbatch[1][number]])
+        plt.imshow(img)
+        plt.show()
+        print('Which has class: ' + str(lab))
+        coord.request_stop()
+        coord.join(threads)
+
+def test_plot(trainbatch,hsize,vsize,number):
+    img_data = (trainbatch[0][number])
+    plt.imshow(img_data)
+    plt.show()
+    print('Which has class: ' + str(trainbatch[1][number]))
+
+#For Saving the Graph NOT IMPLEMENTED FULLY YET
+#def savegraph():
+
+
